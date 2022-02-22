49a4cabe2d6115f054e3b914a08831103dfb0593
aakashbiswas <54617989+aakashbiswas@users.noreply.github.com>
2022-02-22 13:45:28 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Job hai to hai phir kya farak parta hai");
+System.out.println("Change the game");
 	}
 
 }
